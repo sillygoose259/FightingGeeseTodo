32e60593d827ce809624d8199574862ce4364705
lizalvarez <li.alvarez760@gamil.com>
2015-03-06 14:01:51 -0800

    Trying to do fragment

--- app/src/main/java/android/fightinggeese/todo/TodoActivity.java
+++ app/src/main/java/android/fightinggeese/todo/TodoActivity.java
@@ -1,19 +1,37 @@
 package android.fightinggeese.todo;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
 
 
 public class TodoActivity extends Activity {
+
+    protected Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_todo);
-    }
 
+        button = (Button)findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                {
+                    Toast toast = Toast.makeText(getApplicationContext(), "Button Clicked", Toast.LENGTH_SHORT);
+                    toast.show();
+                }
+            }
+        });
+
+      }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
